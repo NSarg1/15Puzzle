1319da742bcd25e2fd7aca766bbd063447028a48
NSarg7 <NSarg7@users.noreply.github.com>
2019-11-22 22:11:54 +0400

    Updates

---
+++ precache-manifest.40a9a41ae6ba586c5bbdc1b6b282e94d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ae10bb32f4cf76ab5255d8b21c3e32",
+    "url": "/15Puzzle/index.html"
+  },
+  {
+    "revision": "c8ac97addfabeff0de1d",
+    "url": "/15Puzzle/static/css/main.32e89b5d.chunk.css"
+  },
+  {
+    "revision": "346f61a338614c8e0404",
+    "url": "/15Puzzle/static/js/2.0ead719f.chunk.js"
+  },
+  {
+    "revision": "c8ac97addfabeff0de1d",
+    "url": "/15Puzzle/static/js/main.7dbbe785.chunk.js"
+  },
+  {
+    "revision": "79e544c55eeea25a962b",
+    "url": "/15Puzzle/static/js/runtime-main.843573b5.js"
+  },
+  {
+    "revision": "004759bbfb1f5371f8593bafc4c89b2f",
+    "url": "/15Puzzle/static/media/Pinewood.004759bb.ttf"
+  },
+  {
+    "revision": "7250d73cb3afa4d3370b9f293593aa6c",
+    "url": "/15Puzzle/static/media/Pinewood.7250d73c.woff"
+  },
+  {
+    "revision": "35e206d3dc0d7c5afac5d88d248e7574",
+    "url": "/15Puzzle/static/media/Tile-1.35e206d3.png"
+  },
+  {
+    "revision": "fc38968e74ffebb6d53bc554287212a2",
+    "url": "/15Puzzle/static/media/Tile-2.fc38968e.png"
+  },
+  {
+    "revision": "c0ddf7aab6ec1d607ea2511b4016f6aa",
+    "url": "/15Puzzle/static/media/box.c0ddf7aa.jpg"
+  },
+  {
+    "revision": "74979afd9c31aa5840320e4364f5e450",
+    "url": "/15Puzzle/static/media/frame-2.74979afd.png"
+  }
+]);
